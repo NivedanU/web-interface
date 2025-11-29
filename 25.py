c71f6ad2ea1579d82d537c2f9b0102d49bd681a1
Nvedan.U <nivedannivedan246@gmail.com>
2025-11-29 14:47:00 +0530

    changes made

---
+++ 25.py
@@ -0,0 +1,120 @@
+x=0
+if x:
+    print("True")
+else:
+    print("False")
+
+x=3
+if x>5:
+    print(1)
+elif x==3:
+    print(2)
+else:
+    print(3)
+
+x=7
+if x%2!=0:
+    print('odd')
+else:
+    print('even')
+    
+a=0
+if a==False:
+    print('Zero')
+else:
+    print('One')
+
+x=10
+if x>5:
+    print("High")
+if x>8:
+    print("higher")
+
+print("5"+"6")
+
+print(type(10==10))
+
+x=5
+if x>3:
+    print("A")
+else:
+    print("B")
+
+a==10
+if a==5:
+    print("Five")
+else:
+    print("Ten")
+
+x=4
+if x%2==0:
+    print("Even")
+else:
+    print("odd")
+
+a="10"
+b=5
+print(a*b)
+
+x=3>2
+print(type(x))
+
+a=2
+b="3"
+print(str(a)+b)
+
+print(4*"a")
+
+x=False
+y=2
+print(x+y)
+
+print(type(10/5))
+
+a="3"
+b=3
+print(a+str(b))
+
+a="Python"
+print(a[0])
+
+a=5
+b=2
+print(a+b)
+
+x=7
+y=3
+print(x//y)
+
+a="6"
+b="4"
+print(a+b)
+
+a=5
+b="%"
+print(a*3)
+
+a="hi"
+print(a*3)
+
+a="10"
+b=2
+print(a*b)
+
+x=True
+y=1
+print(x+y)
+
+x=10.5
+print(type(x))
+
+a="5"
+b="4"
+print(a+b)
+
+x=True
+print(type(x))
+
+a=2
+b=5
+print(a**5)
